e74f010335b695ba1f75afb5469875ee8d9856c1
Adi Satheesh <adisatheesh4@gmail.com>
2025-02-01 17:45:08 -0500

    Added more possible gate configurations

--- ansatz.py
+++ ansatz.py
@@ -2,14 +2,20 @@ from qiskit.circuit import ParameterVector
 import networkx as nx
 from qiskit import QuantumCircuit
 
-gates = {0: 'CX', 1: 'CY', 2: 'CZ', 3: 'RX', 4: 'RY', 5: 'RZ'}
+gates = {
+    0: 'CX_UV', 1: 'CY_UV', 2: 'CZ_UV', 
+    3: 'CX_VU', 4: 'CY_VU', 5: 'CZ_VU',
+    6: 'RX_U', 7: 'RY_U', 8: 'RZ_U',
+    9: 'RX_V', 10: 'RY_V', 11: 'RZ_V'
+}
 
-CX = 0
-CY = 1
-CZ = 2
-RX = 3
-RY = 4
-RZ = 5
+# Deprecated TODO: CHECK IF THIS IS STILL NEEDED
+# CX = 0
+# CY = 1
+# CZ = 2
+# RX = 3
+# RY = 4
+# RZ = 5
 
 def build_ansatz(graph: nx.Graph) -> QuantumCircuit:
 
@@ -38,10 +44,22 @@ def build_with_gates(graph: nx.Graph, gates: list[int]):
             elif gate == 2:
                 ansatz.cz(u, v)
             elif gate == 3:
-                ansatz.rx(t, v)
+                ansatz.cx(v, u)
             elif gate == 4:
-                ansatz.ry(t, v)
+                ansatz.cy(v, u)
             elif gate == 5:
-                ansatz.rz(t, v)
+                ansatz.cz(v, u)
+            elif gate == 6:
+                ansatz.rx(t, u)
+            elif gate == 7:
+                ansatz.ry(t, u)
+            elif gate == 8:
+                ansatz.rz(t, u)
+            elif gate == 9:
+                ansatz.rx(t, v)
+            elif gate == 10:
+                ansatz.ry(t, v)
+            elif gate == 11:
+                ansatz.rz(t, v)            
     
     return ansatz
